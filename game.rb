d3cd6846012f5f7145c41c06ec900b2d355fec37
Evgeny <102020480+villanelle01@users.noreply.github.com>
2022-03-29 18:43:21 +0200

    Add files via upload

---
+++ game.rb
@@ -0,0 +1,38 @@
+objects = [
+	"Камень",
+	"Ножницы",
+	"Бумага"
+]
+computer_choise = rand(3)
+
+puts "Давайте сыграем в игру '\Камень, ножницы, бумага'\ "
+puts
+puts "Выберите вариант"
+puts
+puts "1.Камень"
+puts "2.Ножницы"
+puts "3.Бумага"
+puts
+
+players_input = gets.to_i
+puts
+
+unless (players_input > 0) && (players_input <= objects.size)
+	puts "Нужно выбрать вариант 1, 2 или 3"
+	exit
+end
+
+players_input -= 1 
+
+if players_input == computer_choise 
+	puts "Ничья"
+elsif players_input == 0 && computer_choise == 1
+		puts "Вы выиграли! " + "Компьютер выбрал " + "'" + objects [computer_choise] + "'"
+elsif players_input == 1 && computer_choise == 2
+		puts "Вы выиграли! " + "Компьютер выбрал " + "'" + objects [computer_choise] + "'"
+elsif players_input == 2 && computer_choise == 0
+		puts "Вы выиграли! " + "Компьютер выбрал " + "'" + objects [computer_choise] + "'"
+else
+	puts "Победил компьютер " + "У него " + "'" + objects [computer_choise] + "'"
+end
+
